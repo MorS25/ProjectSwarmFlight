2945dd9e3417f05e0b0c9cb5b1e3370d77b14676
Grigorii Posner <grigoriiposner@me.com>
2015-08-13 10:21:32 +0200

    added movement architecture

--- TestSwarm_1/drone.cpp
+++ TestSwarm_1/drone.cpp
@@ -24,3 +24,44 @@ void Drone::matePosInput(QVector3D aMatePos, int aMateID)
     //position of other drones is converted into relative position
     this->mMatesPos[aMateID] = aMatePos - this->mPos;
 }
+
+void Drone::iMove()
+{
+    //test correct swarming (collision/separation)
+    if(swarming())
+    {
+        //check if the queen has given any orders
+        if(remoteControl())
+        {
+            //if swarming is correct and no orders are given, seek target
+            autonomousControl();
+        }
+    }
+
+    this->mPos = this->mPos + this->mMove;
+    emit mSignalNewPos(mPos,msID);
+}
+
+
+bool Drone::swarming()
+{
+    //collision test
+        //first compare own elevation to others
+
+        //if drones have the same elevation, check out latitude and longitude
+
+    return 1;
+}
+
+bool Drone::remoteControl()
+{
+    //remote control by queen will be added in a further version
+    return 1;
+}
+
+bool Drone::autonomousControl()
+{
+    //direction vector is parallel to line between leader and target
+    this->mDirect = mMatesPos[this->mLeaderID] - this->mTPos;
+    return 1;
+}
